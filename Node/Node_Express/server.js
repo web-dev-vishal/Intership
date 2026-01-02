22e3473a5bc74821f3209fd065da96278dc70ff7
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-02 14:45:03 +0530

    node&Express

---
+++ Node/Node_Express/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.json({ success: true })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:8080`)
+})
