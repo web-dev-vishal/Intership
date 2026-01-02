6989adc2ad2ab423a68e17bf60005d2e52bf81e1
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-02 14:49:25 +0530

    node&Express

--- Node/Node_Express/server.js
+++ Node/Node_Express/server.js
@@ -8,11 +8,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.json({ success: true })
-})
-
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:8000`)
 })
