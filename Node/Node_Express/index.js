7768b7075c88bd602870a4447b7e584bb403c8ac
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-02 15:08:43 +0530

    node&Express

---
+++ Node/Node_Express/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.json({ success: true })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
