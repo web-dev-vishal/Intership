790cda057c466d090daaf4660e712f4fb6d658c2
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-02 15:50:15 +0530

    middleware node&Express

---
+++ Node/Node_Express/new/middlewar.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 7000
+
+app.use(express.json())
+app.use(securityCheck);
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const securityCheck = (req,res,next) => {
+    console.log("Security Check done ✅")
+    next();
+}
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:7000`))
