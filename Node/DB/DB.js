fe2a3b6ba820504b96d2e881f66a651a727f9dae
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-08 15:36:01 +0530

    Database

---
+++ Node/DB/DB.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const fs = require("fs");
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.json({ success: true })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
