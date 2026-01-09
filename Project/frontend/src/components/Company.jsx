fb615c1ff1e3ba735333c92659fdecd5c699fff7
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-09 14:39:08 +0530

    Company name page

--- Project/frontend/src/components/Company.jsx
+++ Project/frontend/src/components/Company.jsx
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react";
-import { getCompaines } from "../services/api.js"
+import { getCompaines } from "../services/api.js";
 
-function CompanyList(){
-    const [companies, setCompanies] = useState([]);
+function CompanyList() {
+  const [companies, setCompanies] = useState([]);
 
-    useEffect(()=> {
-        getCompaines().then(setCompanies);
-    }, []);
+  useEffect(() => {
+    getCompaines().then(setCompanies);
+  }, []);
 
-    return (
-        <div>
-            <h3>Applied Companies</h3>
+  return (
+    <div>
+      <h3>Applied Companies</h3>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Company</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {companies.map((c,i) => {
-                        <tr key={i}>
-                            <td>{c.compamu}</td>
-                            <td>{c.status}</td>
-                        </tr>
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {companies.map((c, i) => {
+            <tr key={i}>
+              <td>{c.compamu}</td>
+              <td>{c.status}</td>
+            </tr>;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default CompanyList();
