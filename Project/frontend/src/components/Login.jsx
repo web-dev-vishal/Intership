37da541965ba18bf8b480201027f39d4bdcc24b2
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-09 14:08:39 +0530

    Login Page Done

---
+++ Project/frontend/src/components/Login.jsx
@@ -0,0 +1,30 @@
+const { useState } = require("react");
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+
+  const handleLogin = () => {
+    if (username.trim() === "") {
+      alert("Please enter name");
+      return;
+    }
+    onLogin();
+  };
+
+  return (
+    <div className="container">
+      <h2>Student Login</h2>
+
+      <input
+        type="text"
+        placeholder="Enter Student Name"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login
