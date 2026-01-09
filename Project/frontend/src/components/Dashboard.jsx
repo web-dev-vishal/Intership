a963cab00562c0ca8e8389f053a10973726ee61a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-01-09 14:25:11 +0530

    Dashboard Done

---
+++ Project/frontend/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard(){
+    return (
+        <div className="container">
+            <h2>Placement Tracker Dashboard</h2>
+            <AddCompany/>
+            <Company/>
+        </div>
+    )
+}
+
+export default Dashboard();
--- Project/frontend/src/main.jsx
+++ Project/frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './components/Login.jsx'
+import Dashboard from './components/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Login></Login>
+    <Dashboard></Dashboard>
   </StrictMode>,
 )
